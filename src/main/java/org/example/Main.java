3867b5f118692e703f7c58fff06f877c07f2d1ae
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-14 19:53:05 -0700

    Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Arrays;
 
 import java.util.Date;
 
@@ -8,18 +9,23 @@ public class Main {
     public static void main(String[] args) {
         Date d = new Date(0);
         char g = 'a';
-        String fullname="gigi";
-        String[] ekk={"sss","aa","vv","eee"};
+        String fullname = "gigi tedi bok";
+        String[] ekk = {"sss", "aa", "vv", "eee"};
+
 
         System.out.println(d);
         System.out.println(g);
         System.out.println(fullname);
         System.out.println(ekk.length);
 
-        for (int i = 0; i < ekk.length; i++) {
-            System.out.println(ekk[i]);
+        String[] splited = fullname.split(" ");
+        System.out.println(Arrays.toString(splited));
+        for (String word : splited) {
+            System.out.println(word);
         }
-
+//        for (int i = 0; i < ekk.length; i++) {
+//            System.out.println(ekk[i]);
+//        }
 
 
     }
